be41680e7329d2b7edd22533091abd46cf269044
nagato0519 <nagasushi0519@gmail.com>
2023-10-19 17:27:09 -0500

    change the navbar options based on logged in or not

--- frontend/src/Layout/navbar.js
+++ frontend/src/Layout/navbar.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {Outlet, Link} from "react-router-dom";
-// import signOut from "../SignOut/signOut";
+import signOut from "../SignOut/signOut";
 import "../Layout/navbar.css"
 import C2Image from '../images/logo.jpg'
 import { Auth } from "aws-amplify";
@@ -8,48 +8,68 @@ import { useNavigate } from "react-router-dom";
 
 const Layout = () => {
     let navigate = useNavigate();
-    async function signOut(event){
+    const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+    React.useEffect(() => {
+        checkUser();
+    }, []);
+
+    async function checkUser() {
+        try {
+            await Auth.currentAuthenticatedUser();
+            setIsAuthenticated(true);
+        } catch (error) {
+            setIsAuthenticated(false);
+        }
+    }
+
+    async function signOut(event) {
         event.preventDefault();
-        try{
+        try {
             await Auth.signOut();
-            console.log("Sign out succesfully")
-            navigate("/")
-        }catch (error){
+            console.log("Sign out successfully");
+            navigate("/");
+        } catch (error) {
             console.log('error signing out: ', error);
         }
     }
     
-        return(
-            <><><nav className="navbar navbar-expand-lg" id="main-nav">
-                <a className="navbar-brand" href="#">
-                    <img
-                        src={C2Image}
-                        className="tcu-image"
-                        width="500"
-                        height="500"
-                        alt="" />
-                </a>
-
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg" id="main-nav">
                 <div className="collapse navbar-collapse justify-content-end" id="navbarCollapse">
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <Link to="/home" className="nav-link text-light"> Home</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to="/account" className="nav-link text-light"> Account</Link>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link text-light" onClick={(e) => signOut(e)} id='sign-out'>Sign Out</a>
-                        </li>
+                        {isAuthenticated ? (
+                            <>
+                                {/* menu when user is signed in */}
+                                <li className="nav-item">
+                                    <Link to="/account" className="nav-link text-light"> Account</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-light" onClick={(e) => signOut(e)} id='sign-out'>Sign Out</a>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                {/* menu when user is not signed in */}
+                                <li className="nav-item">
+                                   <Link to="/signIn" className="nav-link text-light"> Company </Link>
+                                </li>
+                                <li className="nav-item">
+                                   <Link to="/signIn" className="nav-link text-light"> Pricing </Link>
+                                </li>
+                           </>
+
+                        )}
                     </ul>
                 </div>
             </nav>
-
-                <Outlet />
-                </>
-                
-                </>
-        )
+            <Outlet />
+        </>
+    );
 }
 
 export default Layout;
