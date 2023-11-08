1e209d6064e85e3d419461ce2ddac6503558b0fb
Taylor Griffin <104051323+imtaylorgriffin@users.noreply.github.com>
2023-11-07 23:38:22 -0600

    /upload expects userId, creates file structure

--- backend/server.js
+++ backend/server.js
@@ -18,19 +18,28 @@ app.use(cors());
 
 
 ///////////// multer setup
-const dir = path.join(__dirname, '/uploads');  //added 11/2
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir, { recursive: true });
-}
 
 
 // req: request object that contains info about incoming request
 // file: infomration about uploaded file
 // cb: callback function that we need to call to tell multer where and how to store the file.
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'uploads')) // 11-22 '/uploads')  // first argument is null because we don’t have any error to report, second is our uploads folder  located in /uploads
-  },
+   destination: function (req, file, cb) {
+    // Check if userId is present
+    const userId = req.body.userId;
+    if (!userId) {
+      cb(null, false);
+    } else {
+      // Create a directory for the user
+      const dir = path.join(__dirname, 'uploads', userId);
+      fs.mkdirSync(dir, { recursive: true });
+
+      // Pass the directory to multer
+      cb(null, dir);
+    }
+  }, //function (req, file, cb) {
+  //   cb(null, path.join(__dirname, 'uploads')) // 11-22 '/uploads')  // first argument is null because we don’t have any error to report, second is our uploads folder  located in /uploads
+  // },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))  // filename = filename + date + file extension
   }
@@ -38,7 +47,15 @@ var storage = multer.diskStorage({
 
 var upload = multer({
   storage: storage,
-  limits: {fileSize: 5 * 1024 * 1024 * 1024} // Arbitrary max limit of 5gb at the moment
+  limits: {fileSize: 5 * 1024 * 1024 * 1024}, // Arbitrary max limit of 5gb at the moment
+  fileFilter: function (req, file, cb) { //added 11-7
+    // Check if userId is present
+    if (!req.body.userId) {
+      cb(null, false);
+    } else {
+      cb(null, true);
+    }
+  }
 });
 
 /////////////  What if in another js file we auto compress audio files when they've been uploaded?
@@ -65,20 +82,41 @@ app.listen(PORT, () => {
 ////////////// Upload endpoint: Stores file in the web server, uploads info to MongoDB, sends to Workstation
 
 app.post("/upload", upload.single('file'), async (req, res) => {
+     
+    // Check if userID is provided
+    const userId = req.body.userId;
+    if (!userId) {
+      return res.status(400).json({ success: false, message: "No userId provided" });
+    }
+
     if (!req.file) {
       res.status(400).json({ success: false, message: "No file uploaded" });
     }
+
+    
 
     try {
       // Create a new Report in MongoDB with the file info we have
       const audioFile = new Report({  //Audio({
         // want a userID passed in
-        audioPath: req.file.path,
+        userId: userId,
         status: 'in progress'
       });
 
       // Save the audio document in MongoDB
       await audioFile.save();   //send to mongoDB Connector
+
+      // Move the file to a new directory with the _id
+      const newDir = path.join(__dirname, 'uploads', req.body.userId, String(audioFile._id));
+      fs.mkdirSync(newDir, { recursive: true });
+      fs.renameSync(req.file.path, path.join(newDir, req.file.filename));
+
+      const newPath = path.join(newDir, req.file.filename);
+
+      // Update the audioPath in MongoDB
+      audioFile.audioPath = newPath;
+      await audioFile.save();
+
       res.status(200).json({ success: true, id: audioFile._id, message: 'File uploaded and Database entry created successfully' });
 
 
@@ -90,7 +128,7 @@ app.post("/upload", upload.single('file'), async (req, res) => {
       const formData = new FormData();
 
       // Create a stream from the file
-      const fileStream = fs.createReadStream(req.file.path);
+      const fileStream = fs.createReadStream(newPath);
 
       // Append the stream to the form data under the 'file' field
       formData.append('file', fileStream);
@@ -113,7 +151,7 @@ app.post("/upload", upload.single('file'), async (req, res) => {
       console.error(error);
 
       // Send a response indicating an error occurred
-      res.status(500).json({ success: false, message: "An error occurred" });  11-22
+      res.status(500).json({ success: false, message: "An error occurred" });  
     } 
 });
 
