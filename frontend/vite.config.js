745eedcb1986d9918a90ebe47f73f82f482dd716
J4ninja <j4nguyen134@gmail.com>
2023-12-29 14:14:26 -0600

    add eslint to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
       // this ensures that the browser opens upon server start
       open: true,
@@ -12,5 +13,8 @@ export default defineConfig({
   },
   define: {
     global: {}
+  },
+  build: {
+    outDir: 'build',
   }
 })
