c3217a6e14a31734ac86e5dedb6871bc89bf9dbf
John Nguyen <j4nguyen134@gmail.com>
2024-02-27 00:44:30 -0600

    remove class to center?

--- frontend/src/components/Main/WhisperModel/FullTranscript.jsx
+++ frontend/src/components/Main/WhisperModel/FullTranscript.jsx
@@ -190,7 +190,7 @@ export default function FullTranscript({
                         autoFocus
                       />
                     ) : (
-                      <div className="transcript-text">{sentence.text}</div>
+                      <div>{sentence.text}</div>
                     )}
                   </td>
                 </tr>
