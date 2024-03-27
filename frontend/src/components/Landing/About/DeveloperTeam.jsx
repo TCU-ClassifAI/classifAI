64e3c008afd6b5fab7ddfb73e79bb2918a3eef8e
nagato0519 <nagasushi0519@gmail.com>
2024-03-26 23:03:36 -0500

    add nagato

--- frontend/src/components/Landing/About/DeveloperTeam.jsx
+++ frontend/src/components/Landing/About/DeveloperTeam.jsx
@@ -2,6 +2,7 @@ import styles from "./about.module.css"; // Import the CSS file for styling
 import generic from "../../../images/genericprofile.jpg";
 import TaylorGriffin from "../../../images/TaylorGriffin.png";
 import JohnHenryMejia from "../../../images/JohnHenryMejia.jpg";
+import NagatoKadoya from "../../../images/NagatoKadoya.png";
 import JohnNguyen from "../../../images/johnheadshot2.jpeg";
 
 export default function DeveloperTeam() {
@@ -34,7 +35,7 @@ export default function DeveloperTeam() {
       name: "Nagato Kadoya",
       role: "Developer",
       description: `A senior computer science student. Responsible for frontend written in react.js`,
-      imageUrl: generic,
+      imageUrl: NagatoKadoya,
     },
   ];
   return (
